233fda29ca1e7d9497b9b07cc1317fe57ceaca95
Jayper Sanchez <jaypersanchez@gmail.com>
2025-04-15 06:25:09 +0800

    start script will show branch in pm2 list 6 attempt

--- start-app.sh
+++ start-app.sh
@@ -25,7 +25,7 @@ kill -9 $(lsof -t -i :5001) || true
 pm2 delete scoutjar-ai-mvp0.1 || true
 
 # Start the Flask app with pm2
-pm2 start "python3 app.py" --name "scoutjar-ai-mvp0.1"
+pm2 start "python3 app.py" --name "scoutjar-ai-mvp0.1" --no-autorestart --time
 
 # Save the pm2 process list
 pm2 save
